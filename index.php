7a217bd5d7c21c96a11f33032bfe9dca38a438d1
Dmitriy Balovnev <gooddimoh@gmail.com>
2020-01-15 21:31:50 +0200

    last update

--- index.php
+++ index.php
@@ -9,18 +9,19 @@ if (!$connection) {
 if (strtolower(isset($_GET['action'])) === "login") {
     mysqli_fetch_all(mysqli_query($connection, 'INSERT into `users` (`lastname`,`firstname`) VALUES (`lastname`, `firstname`, `password`, `timestamp`)'));
     $res = mysqli_fetch_all(mysqli_query($connection, 'SELECT into `users` (`lastname`,`firstname`) VALUES (`lastname`, `firstname`, `password`, `timestamp`)'));
-
-    in_array($_GET["email"], $res);
-    if ($res["email"] == " ") {
-        mysqli_query($connection, 'INSERT into `users` VALUES(`lastname`, `firstname`, `password`, `timestamp`)');
+    if (in_array($_GET["email"], $res) && in_array($_GET["email"], $res) == "") {
+        if ($res["email"] == " ") {
+            mysqli_query($connection, 'INSERT into `users` VALUES(`lastname`, `firstname`, `password`, `timestamp`)');
+        }
     }
 }
 
 if (strtolower(isset($_GET['action'])) === "signup") {
     mysqli_fetch_all(mysqli_query($connection, 'SELECT `login`, `password` FROM `users`'));
-    in_array($_GET["email"], $res);
-    if ($res["email"] == " ") {
-        mysqli_query($connection, 'INSERT into `users` VALUES(`lastname`, `firstname`, `password`, `timestamp`)');
+    if (!in_array($_GET["email"], $res)) {
+        if ($res["email"] == " ") {
+            mysqli_query($connection, 'INSERT into `users` VALUES(`lastname`, `firstname`, `password`, `timestamp`)');
+        }
     }
 }
 
@@ -29,12 +30,13 @@ mysqli_close($connection);
 
 
 if (strtolower(isset($_GET['action'])) === "graphapi") {
-
-
     $res = mysqli_fetch_all(mysqli_query($connection, "SELECT id, email FORM `users`", MYSQLI_ASSOC));
-    var_dump($res);
     header('Cache-Control: no-cache, must-revalidate');
     header('Content-type: application/json');
+
+    foreach ($res as $index => $item) {
+
+    }
 
     echo json_encode($result_json);
 }
--- login.sql
+++ login.sql
@@ -14,6 +14,6 @@ CREATE TABLE Users (
 INSERT into users VALUES (lastname, firstname, password, timestamp) WHERE login != email
 -- Login --
 
--- Signup --
+-- Sign-up --
 SELECT login, password FROM users WHERE login LIKE login and password LIKE password
--- Signup --
+-- Sign-up --
